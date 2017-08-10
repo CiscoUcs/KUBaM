eb0194ba76053ac6615d2717e4604282bab56b71
vallard <vallard@benincosa.com>
2017-08-09 22:27:24 -0700

    finished initial docker image that makes bootable directory for stage1.  Just needs kickstart files.

--- stage1/scripts/init.sh
+++ stage1/scripts/init.sh
@@ -23,15 +23,35 @@ fi
 cd $KUBAM_ROOT
 # osirrox is so rad!  
 # thanks: https://stackoverflow.com/questions/22028795/is-it-possible-to-mount-an-iso-inside-a-docker-container
-if [ ! -d isolinux ] 
+if [ ! -d centos7.3 ] 
 then
-  echo "Extracting isolinux file..."
-  osirrox -prog kubam -indev ./*.iso -extract . isolinux
+  echo "Extracting centos 7.3 ISO..."
+  osirrox -prog kubam -indev ./*.iso -extract . centos7.3
   echo "Finished extracting"
 else
-   echo "isolinux file exists."
+   echo "centos7.3 directory exists."
 fi
 # extract the file in the kubam directory
+
+if [ ! -d stage1 ]
+then
+  echo "Making installation image."
+  mkdir -p stage1
+  cp -a centos7.3/isolinux stage1/
+  cp -a centos7.3/.discinfo stage1/isolinux/
+  cp -a centos7.3/LiveOS stage1/isolinux/
+  cp -a centos7.3/images/  stage1/isolinux/
+  cp /usr/share/kubam/stage1/centos7.3/isolinux.cfg stage1/isolinux/
+fi
+
+if [ ! -e centos7.3-boot.iso ]
+then 
+  echo "Compressing Installation image."
+  mkisofs -o $KUBAM_ROOT/centos7.3-boot.iso -b isolinux.bin \
+  -c boot.cat -no-emul-boot -V 'CentOS 7 x86_64' \
+  -boot-load-size 4 -boot-info-table -r -J -v -T stage1/isolinux
+fi
+
 
 # run the installation script on the ISO file and start the web server. 
 # start nginx  
