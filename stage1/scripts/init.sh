6a8b23ce74e742cea6421208036b9030f25d2ab7
vallard <vallard@benincosa.com>
2017-08-09 20:37:48 -0700

    updated to have nginx work with autoindexing.

--- stage1/scripts/init.sh
+++ stage1/scripts/init.sh
@@ -15,16 +15,25 @@ fi
 KUBAM_ROOT=/usr/share/nginx/html/kubam
 
 # link to the web server directory.
-ln -sf /kubam $KUBAM_ROOT
+if [ ! -d $KUBAM_ROOT ]
+then 
+  ln -sf /kubam $KUBAM_ROOT
+fi
 
-echo "Extracting isolinux file..."
 cd $KUBAM_ROOT
 # osirrox is so rad!  
 # thanks: https://stackoverflow.com/questions/22028795/is-it-possible-to-mount-an-iso-inside-a-docker-container
-osirrox -indev ./*.iso -extract . isolinux
-echo "Finished extracting"
+if [ ! -d isolinux ] 
+then
+  echo "Extracting isolinux file..."
+  osirrox -prog kubam -indev ./*.iso -extract . isolinux
+  echo "Finished extracting"
+else
+   echo "isolinux file exists."
+fi
 # extract the file in the kubam directory
 
 # run the installation script on the ISO file and start the web server. 
 # start nginx  
+echo "starting web server"
 /usr/sbin/nginx 
