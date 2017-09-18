42930b0722f330418db2cdebe89a26e3ec5ce2d2
vallard <vallard@benincosa.com>
2017-09-18 15:42:52 -0700

    added CORS support for GUIs.

--- api-server/app/app.py
+++ api-server/app/app.py
@@ -123,9 +123,19 @@ def deploy_server_autoinstall_images():
     if not err == 0:
         return jsonify({"error": msg})
     return jsonify({"status": "ok"}), 201
-    
+
+# add cors for gui:
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    if request.method == 'OPTIONS':
+        response.headers['Access-Control-Allow-Methods'] = 'DELETE, GET, POST, PUT'
+        headers = request.headers.get('Access-Control-Request-Headers')
+        if headers:
+            response.headers['Access-Control-Allow-Headers'] = headers
+    return response    
+
+app.after_request(add_cors_headers)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
 
