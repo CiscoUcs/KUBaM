b049ecba7bbe804bfa2deff1cbd53f9fbced2c37
vallard <vallard@benincosa.com>
2018-04-25 16:44:49 +0200

    kubam: added networks API and test cases.

--- kubam/app/network/network.py
+++ kubam/app/network/network.py
@@ -9,56 +9,58 @@ network = Blueprint("network", __name__)
 class Network(object):
     # list the server group information
     @staticmethod
-    def list_hosts():
+    def list():
         """
-        / basic test to see if site is up.
-        should return { 'status' : 'ok'}
+        Lists all the network groups
         """
-        err, msg, host_list = YamlDB.list_hosts(Const.KUBAM_CFG)
+        err, msg, net_list = YamlDB.list_network_group(Const.KUBAM_CFG)
         if err == 1:
             return {'error': msg}, 500
-        return {"hosts": host_list}, 200
+        return {"networks": net_list}, 200
 
     # create a new server group
     @staticmethod
-    def create_hosts(req):
+    def create(req):
         """
-        Create a new host entry
-        Format of request should be JSON that looks like:
-        {"name", "aci01", "credentials" : {"user": "admin", "password" : "secret-password", "ip" : "172.28.225.163" }, ...}
+        Create a new network group
         """
-        err, msg = YamlDB.new_hosts(Const.KUBAM_CFG, req)
+        err, msg = YamlDB.new_network_group(Const.KUBAM_CFG, req)
         if err == 1:
             return {'error': msg}, 400
         return {'status': "Network %s created!" % req["name"]}, 201
 
     @staticmethod
-    def update_hosts(req):
-        # TODO Complete me!
+    def update(req):
+        """
+        Update Netowork settings of one of the network groups.
+        """
+        err, msg = YamlDB.update_network_group(Const.KUBAM_CFG, req)
+        if err == 1:
+            return {'error': msg}, 400
         return {"status": "ok"}, 200
 
     @staticmethod
-    def delete_hosts(req):
+    def delete(req):
         """
-        Delete the ACI group from the config.
+        Delete the network group from the config.
         """
         uuid = req['id']
-        err, msg = YamlDB.delete_hosts(Const.KUBAM_CFG, uuid)
+        err, msg = YamlDB.delete_network(Const.KUBAM_CFG, uuid)
         if err == 1:
             return {'error': msg}, 400
         else:
             return {'status': "Network deleted"}, 201
 
 
-@network.route(Const.API_ROOT2 + "/network", methods=['GET', 'POST', 'PUT', 'DELETE'])
+@network.route(Const.API_ROOT2 + "/networks", methods=['GET', 'POST', 'PUT', 'DELETE'])
 @cross_origin()
-def host_handler():
+def network_handler():
     if request.method == 'POST':
-        j, rc = Network.create_hosts(request.json)
+        j, rc = Network.create(request.json)
     elif request.method == 'PUT':
-        j, rc = Network.update_hosts(request.json)
+        j, rc = Network.update(request.json)
     elif request.method == 'DELETE':
-        j, rc = Network.delete_hosts(request.json)
+        j, rc = Network.delete(request.json)
     else:
-        j, rc = Network.list_hosts()
+        j, rc = Network.list()
     return jsonify(j), rc
