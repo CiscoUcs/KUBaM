e8def2854d29ff66a44010d59e3e1580b38b6fc7
vallard <vallard@benincosa.com>
2018-05-03 23:12:42 +0200

    added automation in startnet and winkubam to generate images.

--- kubam/scripts/windows/startnet.cmd
+++ kubam/scripts/windows/startnet.cmd
@@ -1,4 +1,4 @@
-@echo off
+:: OSVER and KUBAM are veriables added by winkubam.bat 
 start /min cmd
 echo Initializing KUBAM WinPE please wait.
 wpeinit
@@ -12,16 +12,15 @@ for /f "delims=" %%a in ('Type "%File2Read%"') do (
 echo Setting IP address of node...
 netsh interface ip set address eth0 static !Line[1]! !Line[2]! !Line[3]!
 :noping
-ping -n 1 172.28.225.135 2> NUL | find "TTL=" > NUL || goto :noping
+ping -n 1 %KUBAM% 2> NUL | find "TTL=" > NUL || goto :noping
 md \kubam
-echo Waiting for successful mount of \\172.28.225.135\kubam (if this hangs, check that samba is running)
+echo "Waiting for successful mount of \\%KUBAM%\kubam (if this hangs, check that samba is running. See: https://ciscoucs.github.io/site/kubam/configure/windows2.html)"
 :nomount
-::net use i: \\%KBAM%\kubam || goto :nomount
-net use i: \\172.28.225.135\kubam || goto :nomount
+net use i: \\%KUBAM%\kubam || goto :nomount
 echo Successfully mounted \\172.28.225.135\install, moving on to execute remote script
 if exist  c:\autounattend.xml copy c:\autounattend.xml x:\kubam\autounattend.xml
 if not exist x:\kubam\autounattend.xml echo I could not find my autoinst file
 if not exist x:\kubam\autounattend.xml pause
-i:\win2012r2\setup /unattend:x:\kubam\autounattend.xml /noreboot
+i:\%OSVER%\setup /unattend:x:\kubam\autounattend.xml /noreboot
 wpeutil reboot
 :end
--- kubam/scripts/windows/winkubam.bat
+++ kubam/scripts/windows/winkubam.bat
@@ -1,30 +1,32 @@
 @echo off
-::This script is used to create customized Winpe and BCD for deployment of Windows Server 2012
+::This script is used to create customized Winpe and BCD for deployment of Windows Server
 ::This script was originally found in xCAT for windows:
 ::https://raw.githubusercontent.com/xcat2/xcat-core/master/KUBAM-server/share/xcat/netboot/windows/genimage.cmd
 ::This script requires that the ADK must be installed onto the Windows 
-::This script can accept three parameters: 
-::winkubam.bat arch [winpe name] [bcdonly]
+::This script can accept two parameters: 
+::winkubam.bat [osversion] [kubamIP]
+::Example:  .\winkubam.bat win2016 172.26.225.135
+
 
 ::get the arch from first param
-set ARCH=%1%
-if [%ARCH%] EQU [x86] set SUFFIX=32
-if [%ARCH%] EQU [amd64] set SUFFIX=64
-if [%SUFFIX%] EQU [] goto :errorbadargs
+set OS=%1%
+set ARCH=amd64
+set SUFFIX=64
+:: only support 2 windows versions at the moment. 
+if [%OS%] EQU [win2012r2] set OSVER=win2012r2
+if [%OS%] EQU [win2016] set OSVER=win2016
+if [%OSVER%] EQU [] goto :errorbadargs
+
+::get the KUBAM server
+set KUBAM=%2%
+if [%KUBAM%] EQU [] goto :errorbadargs
 ::Configuration section
 ::the drive to use for holding the image
 set defdrive=%SystemDrive%
 
 ::get the name of winpe
 set WINPENAME=
-set BCDONLY=
 set BOOTPATH=Boot
-
-if [%1] EQU [] goto :errorbadargs
-if [%2] EQU [] ( echo Generate winpe to default path %defdrive%\WinPE_%SUFFIX%\media ) else  set WINPENAME=%2%
-if [%2] NEQ [] echo Generate winpe to path %defdrive%\WinPE_%SUFFIX%\media\winboot\%WINPENAME%
-if [%3] EQU [bcdonly] set BCDONLY=1
-if [%WINPENAME%] NEQ [] set BOOTPATH=winboot\%WINPENAME%\Boot
 
 ::location where Windows PE from ADK install is located
 set adkpedir=%defdrive%\Program Files (x86)\Windows Kits\8.1\Assessment and Deployment Kit\Windows Preinstallation Environment
@@ -35,10 +37,9 @@ set Path=C:\Program Files (x86)\Windows Kits\8.1\Assessment and Deployment Kit\D
 set Path=C:\Program Files (x86)\Windows Kits\8.1\Assessment and Deployment Kit\Windows Preinstallation Environment;%Path%
 set Path=C:\Program Files (x86)\Windows Kits\8.1\Assessment and Deployment Kit\Deployment Tools\amd64\Oscdimg;%Path%
 
-
 ::clean the c:\winPE_amd64 and copy it from ADK
 if exist %defdrive%\WinPE_%SUFFIX% rd %defdrive%\WinPE_%SUFFIX% /s /q
-if exist %defdrive%\WinKUBAM.iso del %defdrive%\WinKUBAM.iso
+if exist %defdrive%\WinPE_%OSVER%.iso del %defdrive%\WinPE_%OSVER%.iso
 set retpath=%cd%
 cd /d "%adkpedir%"
 call copype.cmd %ARCH% %defdrive%\WinPE_%SUFFIX%
@@ -61,13 +62,15 @@ bcdedit /store %defdrive%\WinPE_%SUFFIX%\media\Boot\BCD.%SUFFIX%
 if [%ARCH%] EQU [x86] copy %defdrive%\WinPE_%SUFFIX%\media\Boot\BCD.%SUFFIX% %defdrive%\WinPE_%SUFFIX%\media\Boot\B32
 if [%ARCH%] EQU [amd64]  copy %defdrive%\WinPE_%SUFFIX%\media\Boot\BCD.%SUFFIX% %defdrive%\WinPE_%SUFFIX%\media\Boot\BCD
 
-::skip the creating of winpe
-if [%BCDONLY%] EQU [1] goto :reorgpath
 
 dism /mount-image /imagefile:%defdrive%\WinPE_%SUFFIX%\media\Sources\boot.wim /index:1 /mountdir:%defdrive%\WinPE_%SUFFIX%\mount
 cd /d %retpath%
-copy startnet.cmd %defdrive%\WinPE_%SUFFIX%\mount\Windows\system32
-rem copy "%defdrive%\Program Files\Windows AIK\Tools\%ARCH%\imagex.exe" %defdrive%\WinPE_%SUFFIX%\mount\Windows\system32
+echo @echo off > startnet.cmd.new
+echo set KUBAM=%KUBAM% >>startnet.cmd.new
+echo set OSVER=%OSVER% >>startnet.cmd.new
+type startnet.cmd >>startnet.cmd.new
+copy startnet.cmd.new %defdrive%\WinPE_%SUFFIX%\mount\Windows\system32
+::mv /y startnet.cmd.new %defdrive%\WinPE_%SUFFIX%\mount\Windows\system32\startnet.cmd
 dism /Image:%defdrive%\WinPE_%SUFFIX%\mount /add-package /packagepath:"%adkpedir%\amd64\WinPE_OCs\WinPE-WMI.cab"
 dism /Image:%defdrive%\WinPE_%SUFFIX%\mount /add-package /packagepath:"%adkpedir%\amd64\WinPE_OCs\WinPE-Scripting.cab"
 dism /Image:%defdrive%\WinPE_%SUFFIX%\mount /add-package /packagepath:"%adkpedir%\amd64\WinPE_OCs\WinPE-RNDIS.cab"
@@ -90,17 +93,10 @@ dism /Unmount-Wim /commit /mountdir:%defdrive%\WinPE_%SUFFIX%\mount
 move %defdrive%\WinPE_%SUFFIX%\media\Sources\boot.wim %defdrive%\WinPE_%SUFFIX%\media\Boot\WinPE_%SUFFIX%.wim
 
 
-::move the c:\WinPE_64\media to c:\WinPE_64\media\winboot\<winpe name> if <winpe name> is specified (second param)
-:reorgpath
-if [%WINPENAME%] NEQ [] rename %defdrive%\WinPE_%SUFFIX%\media origmedia
-if [%WINPENAME%] NEQ [] md %defdrive%\WinPE_%SUFFIX%\media\winboot
-if [%WINPENAME%] NEQ [] move %defdrive%\WinPE_%SUFFIX%\origmedia %defdrive%\WinPE_%SUFFIX%\media\winboot
-if [%WINPENAME%] NEQ [] rename %defdrive%\WinPE_%SUFFIX%\media\winboot\origmedia %WINPENAME%
-
 echo Finished generating of winpe and BCD.
 echo Generating ISO image.
-MakeWinPEMedia.cmd /ISO %defdrive%\WinPE_64\ %defdrive%\WinKUBAM.iso
-echo "Upload %defdrive%\WinKUBAM.iso into ~/kubam directory of KUBAM (usually /tftpboot/)"
+MakeWinPEMedia.cmd /ISO %defdrive%\WinPE_64\ %defdrive%\WinPE_%OSVER%.iso
+echo "Upload %defdrive%\WinPE_%OSVER%.iso into ~/kubam directory of KUBAM"
 goto :eof
 :errorbadargs
 echo Specify the architecture on the command line
