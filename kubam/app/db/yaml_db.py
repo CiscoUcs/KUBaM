01af547c4e8f547b5c63c926ba398301f5811f7d
vallard <vallard@benincosa.com>
2018-05-18 12:32:27 +0200

    kubam: keep fixing test cases

--- kubam/app/db/yaml_db.py
+++ kubam/app/db/yaml_db.py
@@ -1077,8 +1077,8 @@ class YamlDB(object):
                 if "hosts" in config:
                     for host in config['hosts']:
                         if "network_group" in host:
-                            if host['network_group'] == group['name']:
-                                return 1, "Can't delete network_group: {0} is using it.".format(host['name'])
+                            if host['network_group'] == nname:
+                                return 1, "Can't delete network_group: {0} is using it.".format(nname)
                 config['network_groups'].remove(group)
                 break
         # Now that it is removed, write the config file back out
