54a394f869acb9343ce35f81681c84e9e6482b41
vallard <vallard@benincosa.com>
2018-05-25 14:00:04 -0700

    added socket inspection to connect to ucs central for more error detection.

--- kubam/app/ucsc/ucsc_session.py
+++ kubam/app/ucsc/ucsc_session.py
@@ -10,6 +10,16 @@ class UCSCSession(object):
 
     # Returns handle or error message
     def login(self, username, password, server):
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(10)
+        try:
+            result = s.connect_ex((server, 443))
+            if result != 0:
+                return None, "{0} on port 443 is not reachable.".format(server)
+            s.close()
+        except socket.error as err:
+            return None, "UCS Central connection error: {0} {1}".format(server, err.strerror)
+            
         handle = UcscHandle(server, username, password)
         try:
             handle.login()
@@ -22,7 +32,7 @@ class UCSCSession(object):
         except Exception as e:
             print "Issue logging in. Please check that all parameters are correct"
             print e
-            return None, "Issue logging in. Please check that all parameters are correct."
+            return None, "Issue logging in: {0}".format(str(e))
 
         return handle, None
 
