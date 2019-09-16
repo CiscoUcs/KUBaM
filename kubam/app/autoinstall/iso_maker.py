3cd69803ace42bb25d5b52e16042ebbd24fb04e1
jorge.herrero <jorge.herrero.garcia@bbva.com>
2019-09-16 18:20:39 +0200

    MakeBoot IsoMaker rhvh4.3

--- kubam/app/autoinstall/iso_maker.py
+++ kubam/app/autoinstall/iso_maker.py
@@ -213,6 +213,7 @@ class IsoMaker(object):
             return IsoMaker.mkboot_centos("rhvh", "4.1")
         elif oper_sys == "rhvh4.1":
             return IsoMaker.mkboot_centos("rhvh", "4.3")
+        elif oper_sys == "rhvh4.3":
         elif oper_sys in ["win2016", "win2012r2"]:
             return IsoMaker.mkboot_winpe()
         return 0, "success"
